f31342deea46f245c39ff973166d2ac3660bcff8
Colin-Stark <collinscodes@gmail.com>
2024-10-21 11:49:16 -0400

    changed vercel

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ require('dotenv').config();
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send(`${process.env.key}`);
+    res.send(`${process.env.KEY}`);
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
 
 module.exports = app;
